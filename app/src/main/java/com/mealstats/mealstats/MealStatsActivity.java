5fa904f639a929856c6c6418a8a328b09a8db9be
milder <milderhc@gmail.com>
2016-09-10 03:03:40 -0500

    Initial commit

---
+++ app/src/main/java/com/mealstats/mealstats/MealStatsActivity.java
@@ -0,0 +1,13 @@
+package com.mealstats.mealstats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MealStatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_stats);
+    }
+}
