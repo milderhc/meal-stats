7822d7e7c4f774552efbb9afd003ee6b0a354f22
milder <milderhc@gmail.com>
2016-11-01 17:10:29 -0500

    Add facebook login

--- app/src/main/java/com/mealstats/mealstats/controller/LoginActivity.java
+++ app/src/main/java/com/mealstats/mealstats/controller/LoginActivity.java
@@ -7,15 +7,56 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.mealstats.mealstats.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    /*
+    * Facebook Login
+    * */
+
+    private CallbackManager callbackManager;
+
+    public void facebookLogin () {
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.facebook_login_button);
+        loginButton.setReadPermissions("public_profile");
+        loginButton.setReadPermissions("email");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        facebookLogin();
     }
+
+
 
     public void goToMainActivity(View view) {
         startActivity(new Intent(this, MealStatsActivity.class));
