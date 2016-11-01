f9cce28a055b14c7aee7ec60fcf90df6fdc0747d
milder <milderhc@gmail.com>
2016-11-01 15:46:26 -0500

    Add login activity

---
+++ app/src/main/java/com/mealstats/mealstats/controller/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.mealstats.mealstats.controller;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.mealstats.mealstats.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void goToMainActivity(View view) {
+        startActivity(new Intent(this, MealStatsActivity.class));
+    }
+}
